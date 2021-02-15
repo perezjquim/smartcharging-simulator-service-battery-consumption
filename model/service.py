dcb2f0355af22b106ea8d7652c6141df91ee4a65
Joaquim Perez <jquim1@hotmail.com>
2021-02-15 22:22:42 +0000

    refactor

--- model/service.py
+++ model/service.py
@@ -12,38 +12,38 @@ class ModelService:
     BATTERY_CONSUMPTION_PER_KM_AVG = 0.504
     BATTERY_CONSUMPTION_PER_KM_STDDEV = 0.676
 
-    name = 'model_battery_consumption'
+    name = 'model_energysim_travel_final_battery_level'
 
-    event_dispatcher = EventDispatcher()
+    event_dispatcher = EventDispatcher( )
 
     @rpc
-    def get_final_battery_level(self, initial_battery_level, travel_distance):
-        battery_consumption_per_km = self.generate_battery_consumption_per_km( )
-        final_battery_level = initial_battery_level - ( float( travel_distance ) * battery_consumption_per_km )
+    def get_final_battery_level( self, initial_battery_level, travel_distance ):
+        travel_battery_consumption = self.generate_battery_consumption( )
+        final_battery_level = initial_battery_level - ( float( travel_distance ) * travel_battery_consumption )
 
-        response = json.dumps({'final_battery_level': final_battery_level})
+        response = json.dumps( { 'final_battery_level': final_battery_level } )
         return response
 
-    def generate_battery_consumption_per_km(self):
+    def generate_battery_consumption(self):
         shape = [ 1,1 ]
-        min_battery_consumption_per_km = self.BATTERY_CONSUMPTION_PER_KM_AVG - self.BATTERY_CONSUMPTION_PER_KM_STDDEV
-        max_battery_consumption_per_km = self.BATTERY_CONSUMPTION_PER_KM_AVG + self.BATTERY_CONSUMPTION_PER_KM_STDDEV
+        min_battery_consumption = ModelService.BATTERY_CONSUMPTION_PER_KM_AVG - ModelService.BATTERY_CONSUMPTION_PER_KM_STDDEV
+        max_battery_consumption = ModelService.BATTERY_CONSUMPTION_PER_KM_AVG + ModelService.BATTERY_CONSUMPTION_PER_KM_STDDEV
 
         tf_random = tf.random.uniform(
                 shape=shape,
-                minval=min_battery_consumption_per_km,
-                maxval=max_battery_consumption_per_km,
+                minval=min_final_battery_level,
+                maxval=max_final_battery_level,
                 dtype=tf.dtypes.float32,
                 seed=None,
                 name=None
         )
         tf_var = tf.Variable( tf_random )
 
-        tf_init = tf.compat.v1.global_variables_initializer()
-        tf_session = tf.compat.v1.Session()
-        tf_session.run(tf_init)
+        tf_init = tf.compat.v1.global_variables_initializer( )
+        tf_session = tf.compat.v1.Session( )
+        tf_session.run( tf_init )
 
-        tf_return = tf_session.run(tf_var)
-        battery_consumption_per_km = float( tf_return[ 0 ][ 0 ] )
+        tf_return = tf_session.run( tf_var )
+        travel_final_battery_level = float( tf_return[ 0 ][ 0 ] )
 
-        return battery_consumption_per_km
+        return travel_final_battery_level
