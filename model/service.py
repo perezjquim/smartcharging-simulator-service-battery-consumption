2f391701badab5f6b435e1ab0c764d61fd66dd48
Joaquim Perez <jquim1@hotmail.com>
2021-04-02 12:35:08 +0100

    tf 1.5 adjustments

--- model/service.py
+++ model/service.py
@@ -29,18 +29,18 @@ class ModelService:
         min_battery_consumption = ModelService.BATTERY_CONSUMPTION_PER_KM_AVG - ModelService.BATTERY_CONSUMPTION_PER_KM_STDDEV
         max_battery_consumption = ModelService.BATTERY_CONSUMPTION_PER_KM_AVG + ModelService.BATTERY_CONSUMPTION_PER_KM_STDDEV
 
-        tf_random = tf.random.uniform(
+        tf_random = tf.random_uniform(
                 shape=shape,
                 minval=min_battery_consumption,
                 maxval=max_battery_consumption,
-                dtype=tf.dtypes.float32,
+                dtype=tf.float32,
                 seed=None,
                 name=None
         )
         tf_var = tf.Variable( tf_random )
 
-        tf_init = tf.compat.v1.global_variables_initializer( )
-        tf_session = tf.compat.v1.Session( )
+        tf_init = tf.global_variables_initializer( )
+        tf_session = tf.Session( )
         tf_session.run( tf_init )
 
         tf_return = tf_session.run( tf_var )
